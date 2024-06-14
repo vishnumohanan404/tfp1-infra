51070c60ad54ce23f7b254031265f492909767ed
Vishnu Mohan <77823657+KingKong26@users.noreply.github.com>
2024-06-14 18:26:04 +0530

    sleep time added

--- infra/template/ec2_install_python_app.sh
+++ infra/template/ec2_install_python_app.sh
@@ -4,7 +4,7 @@ cd /home/ubuntu
 yes | sudo apt update
 yes | sudo apt install python3 python3-pip
 git clone https://github.com/vishnumohanan404/tfp1-python-app.git
-sleep 20
+sleep 30
 # shellcheck disable=SC2164
 cd tfp1-python-app
 echo 'Install requirements'
@@ -13,5 +13,5 @@ pip3 install -r requirements.txt
 echo "RDS endpoint: ${rds_endpoint}"
 export RDS_HOST=${rds_endpoint}
 echo 'Waiting for 30 seconds before running the app.py'
-sleep 30
+sleep 40
 setsid python3 -u app.py &
