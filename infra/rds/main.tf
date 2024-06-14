6a84ce218b576b3b5ab63061156220dd3e601123
Vishnu Mohan <77823657+KingKong26@users.noreply.github.com>
2024-06-14 13:19:55 +0530

    mysql engine version support issue resolved

--- infra/rds/main.tf
+++ infra/rds/main.tf
@@ -16,7 +16,7 @@ resource "aws_db_instance" "default" {
   allocated_storage       = 10
   storage_type            = "gp2"
   engine                  = "mysql"
-  engine_version          = "5.7"
+  engine_version          = "8.0"
   instance_class          = "db.t2.micro"
   identifier              = var.mysql_db_identifier
   username                = var.mysql_username
