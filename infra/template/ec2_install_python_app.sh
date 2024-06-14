a2e9fa0dc5add9d45837d248981e3e6cb8d29526
Vishnu Mohan <77823657+KingKong26@users.noreply.github.com>
2024-06-14 16:19:01 +0530

    echo added

--- infra/template/ec2_install_python_app.sh
+++ infra/template/ec2_install_python_app.sh
@@ -7,6 +7,7 @@ git clone https://github.com/vishnumohanan404/tfp1-python-app.git
 sleep 20
 # shellcheck disable=SC2164
 cd tfp1-python-app
+echo 'Install requirements'
 pip3 install -r requirements.txt
 echo 'Waiting for 30 seconds before running the app.py'
 setsid python3 -u app.py &
