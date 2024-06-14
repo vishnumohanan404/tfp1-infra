1d519ed124dac0b46d01c339534a37070175db8a
Vishnu Mohan <77823657+KingKong26@users.noreply.github.com>
2024-06-15 01:17:25 +0530

    cidr block replaced by security block

--- infra/security-groups/main.tf
+++ infra/security-groups/main.tf
@@ -71,7 +71,7 @@ resource "aws_security_group" "rds_mysql_sg" {
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
-    cidr_blocks = var.public_subnet_cidr_block # replace with your EC2 instance security group CIDR block
+    security_groups = [aws_security_group.ec2_sg_python_api.id]
   }
 }
 
